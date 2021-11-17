352420f60f43bc61973acfa92d9dfec2dd544bad
Michal Strzezon <mstrzezon@gmail.com>
2021-11-17 14:20:54 +0100

    update pitches list

--- src/components/PitchesList.js
+++ src/components/PitchesList.js
@@ -2,9 +2,9 @@ import './MatchesList.css'
 
 function PitchesList( {pitches} ) {
     return(
-        <div className='matches-list'>
+        <div className='pitches-list'>
             {pitches.map((pitches) => (
-                <div className='match-preview' key={pitches.id}>
+                <div className='pitch-preview' key={pitches.id}>
                     <h2> { pitches.name } </h2>
                         <p>Adres: {pitches.address}</p>
                         <p>Kontakt: {pitches.contact}</p>
