96794a666d7f84a42bcca7470200b0bc8639bfb9
Michal Strzezon <mstrzezon@gmail.com>
2021-11-17 14:54:33 +0100

    update match details

--- src/components/MatchDetails.js
+++ src/components/MatchDetails.js
@@ -1,8 +1,8 @@
 import { useParams } from "react-router-dom";
 import useFetch from './useFetch';
-import { useState } from "react";
 import axios from 'axios';
 import {useHistory } from 'react-router-dom';
+import './MatchDetails.css'
 
 const MatchDetails = () => {
     const { id } = useParams();
@@ -18,14 +18,14 @@ const MatchDetails = () => {
 
     return (
         <div className="match_details" >
-            <div className="row">
-                <div className="column">
+            <div className="row1">
+                <div className="column1">
                     <h1>Lokalizacja</h1>
                     <h2> { pitch && pitch.name } </h2>
                     <p>Adres: {pitch && pitch.address}</p>
                     <p>Kontakt: {pitch && pitch.contact}</p>
                 </div>
-                <div className="column">
+                <div className="column1">
                     <h1>Informacja o wydarzeniu</h1>
                     <p>{match && match.description}</p>
                     <p>Data: {match && match.date.slice(0,10)}</p>
